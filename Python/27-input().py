834265ef8e56d0a9dfb3bf5814e6aa88c5b53f48
Francis O'Hara Aidoo <francis-ohara@outlook.com>
2023-02-18 15:11:09 +0000

    add 27-input().py

---
+++ Python/27-input().py
@@ -0,0 +1,17 @@
+# Task
+# You are given a polynomial P of a single indeterminate (or variable), x. You are also given the values of x and k. Your task is to verify if P(x) = k.
+# Constraints
+# All coefficients of polynomial P are integers.
+# x and y are also integers.
+# Input Format
+# The first line contains the space separated values of x and k.
+# The second line contains the polynomial P.
+# Output Format
+# Print True if P(x) = k. Otherwise, print False.
+
+x, k = input().split()
+P = input().replace("x", x)
+if eval(P) == int(k):
+    print(True)
+else:
+    print(False)
