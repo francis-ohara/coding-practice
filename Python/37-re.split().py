6b5cd3943f614d8acd4279456f4cbb606992d511
Francis O'Hara Aidoo <francis-ohara@outlook.com>
2023-03-05 16:01:59 +0000

    Update 37-re.split().py [solved]

--- Python/37-re.split().py
+++ Python/37-re.split().py
@@ -1,6 +1,6 @@
 # Link: https://www.hackerrank.com/challenges/re-split/problem
 # Task:
-# You are given a string s consisting only of digits 0-9, commas „ and dots .
+# You are given a string s consisting only of digits 0-9, commas „ and dots.
 # Your task is to complete the regex_pattern defined below, which will be used to re.split() all the , and . symbols in s.
 # It's guaranteed that every comma and every dot in s is preceded and followed by a digit.
 import re
