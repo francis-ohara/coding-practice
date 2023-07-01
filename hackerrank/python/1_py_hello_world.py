481bf9834693cfc129afa6b833604652e352d7d7
francisohara24 <franciskohara@gmail.com>
2023-07-01 23:06:31 +0000

    update 1_py_hello_world.py

--- hackerrank/python/1_py_hello_world.py
+++ hackerrank/python/1_py_hello_world.py
@@ -1,4 +1,4 @@
-# Link: https://www.hackerrank.com/challenges/py-hello-world/problem
+# link: https://www.hackerrank.com/challenges/py-hello-world/problem
 
 my_string = "Hello, World!"
 print(my_string)
