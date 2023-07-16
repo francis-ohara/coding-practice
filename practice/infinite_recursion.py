54dc3b935b1e5bf9a04751eead81136be6663ca1
francisohara24 <franciskohara@gmail.com>
2023-07-16 22:29:19 +0000

    Style: update comments

--- practice/infinite_recursion.py
+++ practice/infinite_recursion.py
@@ -6,5 +6,6 @@ def function(n):
 
 print(function(1))
 
-# Conclusion Python internally guards against this by throwing an exception called RecursionError whose message is
+# Conclusion:
+# Python internally guards against this by throwing an exception called RecursionError whose message is
 # "maximum recursion depth exceeded".
