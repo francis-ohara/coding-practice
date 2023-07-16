4a6ca05ed42863e8a8d096ee18da03ff972fd509
francisohara24 <franciskohara@gmail.com>
2023-07-16 22:29:48 +0000

    Feat: add python_asssert_statement.py
    script for testing how to use the Python assert statement for unit testing.

---
+++ practice/python_asssert_statement.py
@@ -0,0 +1,15 @@
+"""Test how to use the Python assert statement"""
+
+
+# define function for adding numbers
+def add(a, b):
+    """Return sum of `a` and `b`."""
+    return a + b
+
+
+# test function with assert
+assert add(3, 2) == 5
+
+# Conclusion:
+# No AssertError is raised since add(3, 2) == 5 evaluates to True. If expression after assert is False,
+# an AssertError exception will be raised.
