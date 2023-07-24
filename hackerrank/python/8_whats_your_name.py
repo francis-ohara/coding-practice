80ea3b95cd8c6393176d84bef3ede8946dd562df
francisohara24 <franciskohara@gmail.com>
2023-07-24 20:40:28 +0000

    Feat: add 8_whats_your_name.py

---
+++ hackerrank/python/8_whats_your_name.py
@@ -0,0 +1,7 @@
+# link: https://www.hackerrank.com/challenges/whats-your-name/problem
+
+firstname = input()
+lastname = input()
+
+result = "Hello {} {}! You just delved into python.".format(firstname, lastname)
+print(result)
