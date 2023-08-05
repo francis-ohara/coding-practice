ada17ac1e53e8e88bd86639533bbe43ae7290f36
francisohara24 <franciskohara@gmail.com>
2023-08-05 20:27:08 +0000

    Feat: update 19_find_a_string.py

--- hackerrank/19_find_a_string.py
+++ hackerrank/19_find_a_string.py
@@ -1,2 +1,16 @@
 # link: https://www.hackerrank.com/challenges/find-a-string/problem
 
+string = input()
+sub_string = input()
+count = 0
+
+for i in range(len(string) - len(sub_string) + 1):
+    is_match = True
+    for j in range(len(sub_string)):
+        if string[i + j] != sub_string[j]:
+            is_match = False
+
+    if is_match:
+        count += 1
+
+print(count)
