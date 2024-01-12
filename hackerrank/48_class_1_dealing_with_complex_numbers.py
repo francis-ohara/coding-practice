25fbaf4699b94d0900c219b934a22eb50ddfaa84
francisohara24 <franciskohara@gmail.com>
2024-01-11 23:52:51 -0500

    Docs: add pseudocode as docstring

---
+++ hackerrank/48_class_1_dealing_with_complex_numbers.py
@@ -0,0 +1,17 @@
+# link: https://www.hackerrank.com/challenges/class-1-dealing-with-complex-numbers/problem
+
+"""PSEUDOCODE
+
+CLASS: Complex
+    ATTRIBUTES:
+        1. real: float
+        2. imag: float
+
+    METHODS:
+        1. Constructor(str) -> Complex
+        2. Add (a: Complex, b: Complex) -> Sum: Complex
+        3. Subtract(a: Complex, b: Complex) -> Difference: Complex      // consider positive or negative difference
+        4. Multiply (a: Complex, b: Complex) -> Product: Complex
+        5. Divide (a: Complex, b: Complex) -> Quotient: Complex     // consider division by zero, etc.
+        6. Mod (a: Complex) -> ??
+"""
